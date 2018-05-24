eff105ea706ae7ad49f0072184232b7b5e5afc1c
maolin <772984968@qq.com>
2018-05-24 15:20:16 +0800

    修改

--- app/admin/controller/NewsController.php
+++ app/admin/controller/NewsController.php
@@ -109,7 +109,7 @@ class NewsController extends TemplateController
             }
             if($model->allowField(true)->isUpdate(true)->save($data)){
 
-                $model->city()->sync($data['cities']);
+             //   $model->city()->sync($data['cities']);
                 return  json(['code'=>200,'msg'=>'修改成功']);
             }else{
                 return json(['code'=>400,'msg'=>$model->getError]);
